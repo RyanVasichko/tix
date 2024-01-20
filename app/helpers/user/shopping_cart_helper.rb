b665a7d81aecfa6c3518b1140664fdebc28a5d64
Ryan Vasichko <RyanVasichko@gmail.com>
2024-01-20 11:22:34 -0600

    Fixed turbo-morphing shopping cart

--- app/helpers/user/shopping_cart_helper.rb
+++ app/helpers/user/shopping_cart_helper.rb
@@ -1,16 +1,18 @@
 module User::ShoppingCartHelper
   def morph_shopping_cart
-    concat(turbo_stream.action("morph", "shopping_cart_count") do
+    morphs = []
+    morphs << turbo_stream.action("morph", "shopping_cart_count") do
       render "shopping_cart/count", shopping_cart: Current.user.shopping_cart_with_items
-    end)
+    end
 
-    concat(turbo_stream.action("morph", "shopping_cart") do
+    morphs << turbo_stream.action("morph", "shopping_cart") do
       render "shopping_cart/shopping_cart", shopping_cart: Current.user.shopping_cart_with_items
-    end)
+    end
+
+    safe_join(morphs)
   end
 
   def morph_shopping_cart_and_replace_seat(seat)
-    morph_shopping_cart
-    concat(turbo_replace_seat(seat))
+    safe_join([morph_shopping_cart, turbo_replace_seat(seat)])
   end
 end
