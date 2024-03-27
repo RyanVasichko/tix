03e483f980ddf133fc1507f41e7e11d9574563c6
Ryan Vasichko <RyanVasichko@gmail.com>
2024-03-26 23:13:14 -0500

    Logging

--- app/jobs/order/search_index/populate_job.rb
+++ app/jobs/order/search_index/populate_job.rb
@@ -1,5 +1,5 @@
 class Order::SearchIndex::PopulateJob < ApplicationJob
-  def perform(order_id)
-    Order::SearchIndex.populate_for(Order.find(order_id))
+  def perform(order)
+    Order::SearchIndex.populate_for(order)
   end
 end
---
+++ config/initializers/logging.rb
@@ -0,0 +1,7 @@
+return unless Rails.env.production?
+
+file_logger = ActiveSupport::Logger.new(Rails.root.join("log", "production.log"), 2, 50.megabytes)
+Rails.logger.broadcast_to(file_logger)
+
+error_logger = ActiveSupport::Logger.new(Rails.root.join("log", "production-error.log"), 2, 50.megabytes)
+error_logger.level = Logger::ERROR
