92d2f457aada554b5900996377ade954a9155eb7
Ryan Vasichko <RyanVasichko@gmail.com>
2024-03-28 09:30:25 -0500

    Added HotwireCombobox

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "aws-sdk-s3", require: false
 gem "image_processing", "~> 1.2"
 
+gem "hotwire_combobox"
 gem "bcrypt", "~> 3.1.7"
 gem "pagy"
 
--- app/helpers/admin/shows_helper.rb
+++ app/helpers/admin/shows_helper.rb
@@ -1,2 +1,11 @@
 module Admin::ShowsHelper
+  def artist_combobox_options_for_show(show)
+    artists = if show.persisted?
+                Artist.where(id: show.artist_id)
+              else
+                Artist.active.order(:name)
+              end
+
+    artists.pluck(:name, :id)
+  end
 end
