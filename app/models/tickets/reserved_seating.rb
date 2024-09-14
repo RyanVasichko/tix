20d5a25d5cd7e725720927e2e79d76af7aed1047
Ryan Vasichko <RyanVasichko@gmail.com>
2024-09-14 13:22:46 -0500

    Fixed seating chart live updates

--- app/models/tickets/reserved_seating.rb
+++ app/models/tickets/reserved_seating.rb
@@ -4,9 +4,18 @@ class Tickets::ReservedSeating < Ticket
   has_one :seat, class_name: "Show::Seat", inverse_of: :ticket, touch: true
   delegate :seat_number, :table_number, to: :seat
 
-  after_commit -> { broadcast_replace_later_to [show, "seating_chart"], partial: "shows/reserved_seating/seats/seat", locals: { seat: seat } }
+  after_commit :broadcast_seat_replacement_later
 
   def sold?
     purchase.present?
+  end
+
+  private
+
+  def broadcast_seat_replacement_later
+    broadcast_replace_to [show, "seating_chart"],
+                         target: self.seat,
+                         partial: "shows/reserved_seating/seats/seat",
+                         locals: { seat: seat }
   end
 end
